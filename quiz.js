7def3d15ccccc926d8dfb9e56e04e511dd4e092d
URa-workspace <105509725+URa-workspace@users.noreply.github.com>
2024-01-06 13:20:11 +0300

    Update 06,01 quiz.js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,7 @@
 import { dataQuiz } from "./dataQuiz.js";
+// import { dataClients, addClient } from "./dataClients.js";
+
+
 const quiz = document.getElementById("quizForm");
 const quizFooter = document.querySelector(".quiz__footer");
 const nextBtn = quiz.querySelector(".btnNext");
@@ -10,12 +13,12 @@ const btnSend = document.querySelector(".btn__submit");
 let currentCounter = document.querySelector(".current-item");
 let indexBlock = 1;
 let answer = {
-  name: "",
-  secondName: "",
+  firstName: "",
+  lastName: "",
   tel: null,
-  question2: "",
-  question3: "",
-  question4: "",
+  q2: "",
+  q3: "",
+  q4: "",
 };
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -116,25 +119,25 @@ function addData(quiz) {
   questions.forEach((q) => {
     if (q.dataset.question == "Що вас цікавить?") {
       inps.forEach((i) => {
-        i.checked ? (answer.question2 = i.value) : false;
+        i.checked ? (answer.q2 = i.value) : false;
       });
     } else if (q.dataset.question == "Чому не звернулися раніше?") {
       inps.forEach((i) => {
-        i.checked ? (answer.question3 = i.value) : false;
+        i.checked ? (answer.q3 = i.value) : false;
       });
     } else if (q.dataset.question == "Ваша знижка") {
       inps.forEach((i) => {
-        i.value ? (answer.question4 = i.value) : false;
+        i.value ? (answer.q4 = i.value) : false;
       });
     } else {
       const inps = document.querySelectorAll(".input");
       inps.forEach((i) => {
         switch (i.id) {
-          case "name":
-            answer.name = i.value;
+          case "firstName":
+            answer.firstName = i.value;
             break;
-          case "secondName":
-            answer.secondName = i.value;
+          case "lastName":
+            answer.lastName = i.value;
             break;
           case "tel":
             answer.tel = i.value;
@@ -184,27 +187,130 @@ function addDataToForm(quiz) {
   inps.forEach((inp) => {
     for (let key in answer) {
       switch (key) {
-        case "name":
-          inp.id === "nameUser" ? (inp.value = answer[key]) : false;
+        case "firstName":
+          inp.id === "firstName" ? (inp.value = answer[key]) : false;
           break;
-        case "secondName":
-          inp.id === "secondNameUser" ? (inp.value = answer[key]) : false;
+        case "lastName":
+          inp.id === "lastName" ? (inp.value = answer[key]) : false;
           break;
         case "tel":
-          inp.id === "telUser" ? (inp.value = answer[key]) : false;
+          inp.id === "tel" ? (inp.value = answer[key]) : false;
           break;
-        case "question2":
+        case "q2":
           inp.id === "q2" ? (inp.value = answer[key]) : false;
           break;
-        case "question3":
+        case "q3":
           inp.id === "q3" ? (inp.value = answer[key]) : false;
           break;
-        case "question4":
+        case "q4":
           inp.id === "q4" ? (inp.value = answer[key]) : false;
           break;
       }
-      console.log(`key: ${answer[key]}`);
-      console.log(`inp.id: ${inp.id}`);
+      // console.log(`key: ${answer[key]}`);
+      // console.log(`inp.id: ${inp.id}`);
     }
   });
 }
+
+btnSend ? sendForm() : false;
+
+function sendForm() {
+  btnSend.addEventListener("click", (e) => {
+    e.preventDefault();
+    formSend();
+  });
+}
+
+async function formSend() {
+  let error = formValidate(quiz);
+
+  let formData = new FormData(quiz);
+
+  if (error === 0) {
+    const hideAfterSend = document.querySelectorAll(".hide-after-send");
+    hideAfterSend.forEach((item) => {
+      item.classList.add("hide");
+    });
+
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, "0");
+    let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    let yyyy = today.getFullYear();
+    let ou = today.getHours();
+    let min = today.getMinutes();
+    let sec = String(today.getSeconds()).padStart(2, "0");
+    today = dd + "." + mm + "." + yyyy + " " + ou + ":" + min + ":" + sec;
+    formData.append("id", today);
+    localStorage.setItem(today, JSON.stringify(answer));
+
+    const values = Object.fromEntries(formData.entries());
+
+    document.querySelector(".quiz__response").classList.remove("hide");
+    setTimeout(() => {
+      document.getElementById("pQuiz").classList.remove("open");
+      console.log(values);
+    }, 5000);
+
+    // document.getElementById("popupBody").classList.add("_sending"); 
+
+    // let response = await fetch("./dataClients.js", {
+    //   method: "POST",
+    //   body: formData,
+    // });
+    // if (response.ok) {
+    //   let result = await response.json();
+    //   alert(result.message);
+    //   form.reset();
+    //   document.getElementById("popupBody").classList.remove("_sending");
+    // } else {
+    //   alert("Помилка при відправці!");
+    //   form.reset();
+    //   document.getElementById("popupBody").classList.remove("_sending");
+    // }
+  } else {
+    alert("Уважно заповніть обов'язкові поля!");
+  }
+}
+
+function formValidate(quiz) {
+  let error = 0;
+  let formReq = quiz.querySelectorAll("._req");
+  for (let index = 0; index < formReq.length; index++) {
+    const input = formReq[index];
+    formRemoveError(input);
+    if (input.classList.contains("_tel")) {
+      if (telTest(input)) {
+        formAddError(input);
+        error++;
+      }
+    } else if (input.value === "") {
+      formAddError(input);
+      error++;
+    }
+  }
+  return error;
+}
+
+function formAddError(input) {
+  input.parentElement.classList.add("_error");
+  // input.classList.add("_error");
+}
+function formRemoveError(input) {
+  input.parentElement.classList.remove("_error");
+  // input.classList.remove("_error");
+}
+function telTest(input) {
+  const validValue = /^\+\d[\d\(\)\ -]{10,19}\d$/;
+  return !validValue.test(input.value);
+}
+// const elements = document.querySelectorAll("._tel");
+// if (elements.length > 0) {
+//   for (let i = 0; i < elements.length; i++) {
+//     var element = elements[i];
+//     var maskOptions = {
+//       mask: "+{38}(000)000-00-00",
+//       lazy: false,
+//     };
+//     var mask = new IMask(element, maskOptions);
+//   }
+// }
